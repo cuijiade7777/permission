8c469f25bfd6a7768be6b84eef8ef252f0af6b48
cuijiade7777 <45115197+cuijiade7777@users.noreply.github.com>
2019-01-14 22:19:09 +0800

    6-6 获取Spring上下文工具-ApplicationContextHelper开发

--- src/main/java/com/mmall/controller/TestController.java
+++ src/main/java/com/mmall/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.mmall.controller;
 
+import com.mmall.common.ApplicationContextHelper;
 import com.mmall.common.JsonData;
+import com.mmall.dao.SysAclMapper;
+import com.mmall.dao.SysAclModuleMapper;
 import com.mmall.exception.ParamException;
+import com.mmall.model.SysAclModule;
 import com.mmall.param.TestVo;
 import com.mmall.util.BeanValidator;
+import com.mmall.util.JsonMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +32,9 @@ public class TestController {
     @ResponseBody
     public JsonData validate(TestVo vo) throws ParamException{
         log.info("validate");
+        SysAclModuleMapper moduleMapper = ApplicationContextHelper.popBean(SysAclModuleMapper.class);
+        SysAclModule module = moduleMapper.selectByPrimaryKey(1);
+        log.info(JsonMapper.obj2String(module));
         BeanValidator.check(vo);
         return JsonData.success("text validate");
     }
